6465a507e95a8519e7e093ffb1555e28196a8324
aaron byun <macmath22@gmail.com>
2018-03-03 20:52:15 -0800

    add ui component

--- users-service/project/__init__.py
+++ users-service/project/__init__.py
@@ -1,11 +1,14 @@
 import os
 from flask import Flask, jsonify
 from flask_sqlalchemy import SQLAlchemy
+from flask_cors import CORS
 
 db = SQLAlchemy()
 
 def create_app():
     app = Flask(__name__)
+
+    CORS(app)
 
     app_settings = os.getenv('APP_SETTINGS')
     app.config.from_object(app_settings)
