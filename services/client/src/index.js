767f112bc7cca86796f5c5d4f7679449609d608a
aaron byun <macmath22@gmail.com>
2018-03-11 00:19:28 -0800

    add react to prod

--- services/client/src/index.js
+++ services/client/src/index.js
@@ -9,7 +9,7 @@ class App extends Component {
         super(props);
         this.state = {
             users: [],
-            username: '',
+            username: 'justatest',
             email: ''
         };
 
